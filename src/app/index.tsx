43054c8a454383916c6d4dce0291517222c47a8c
Elián Roventini Andrade <elianroventini@outlook.com.br>
2023-11-22 21:06:13 -0300

    Icons on menu

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,31 +5,38 @@ import {
     Button,
     ButtonText
 } from "@gluestack-ui/themed";
+
+import FontAwesome from '@expo/vector-icons/FontAwesome5';
+
 import { Link } from "expo-router";
 
 export default function MenuScreen() {
     return (
-        <View pt="$20">
-            <Center h="$80" justifyContent="center">
-                <VStack space="md" reversed={false}>
+        <View>
+            <Center justifyContent="center" h="$full">
+                <VStack space="xl" reversed={false}>
                     <Link href="/(tabs)/mapa" asChild>
-                        <Button>
-                            <ButtonText>Mapa</ButtonText>
+                        <Button size="lg" variant="solid">
+                            <FontAwesome name='map' color={'#fff'} size={25} />
+                            <ButtonText pl="$4">Mapa</ButtonText>
                         </Button>
                     </Link>
                     <Link href="/(tabs)/paroquia/" asChild>
-                        <Button>
-                            <ButtonText>Paróquias</ButtonText>
+                        <Button size="lg" variant="solid">
+                            <FontAwesome name='church' color={'#fff'} size={25} />
+                            <ButtonText pl="$4">Paróquias</ButtonText>
                         </Button>
                     </Link>
                     <Link href="/(tabs)/evento/" asChild>
-                        <Button>
-                            <ButtonText>Eventos</ButtonText>
+                        <Button size="lg" variant="solid">
+                            <FontAwesome name='calendar' color={'#fff'} size={25} />
+                            <ButtonText pl="$4">Eventos</ButtonText>
                         </Button>
                     </Link>
                     <Link href="/(tabs)/aviso/" asChild>
-                        <Button>
-                            <ButtonText>Avisos</ButtonText>
+                        <Button size="lg" variant="solid">
+                            <FontAwesome name='book-open' color={'#fff'} size={25} />
+                            <ButtonText pl="$4">Avisos</ButtonText>
                         </Button>
                     </Link>
                 </VStack>
