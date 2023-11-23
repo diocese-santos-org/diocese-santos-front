d252415cfe097e2c6b609af9e0432bb0c38aba30
Elián Roventini Andrade <elianroventini@outlook.com.br>
2023-11-22 21:01:05 -0300

    Showing the map on Mapa Screen with the current location

--- src/app/(tabs)/mapa.tsx
+++ src/app/(tabs)/mapa.tsx
@@ -1,18 +1,84 @@
+import { useState, useEffect } from 'react';
+
 import {
-    Center,
-    VStack,
     View,
     Text
 } from "@gluestack-ui/themed";
 
+import MapView, { Marker, Region } from 'react-native-maps';
+import {
+    LocationObject,
+    requestForegroundPermissionsAsync,
+    getCurrentPositionAsync,
+    watchPositionAsync,
+    LocationAccuracy
+} from 'expo-location';
+
 export default function MapaScreen() {
+    const [location, setLocation] = useState<LocationObject | null>(null);
+    const [region, setRegion] = useState<Region>({
+        latitude: location?.coords.latitude || -23.9562553,
+        longitude: location?.coords.longitude || -46.3170556,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    });
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+    async function requestLocationPermissions() {
+        try {
+            const { status } = await requestForegroundPermissionsAsync();
+
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            setLocation(await getCurrentPositionAsync({}));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        try {
+            requestLocationPermissions();
+
+            watchPositionAsync({
+                accuracy: LocationAccuracy.High,
+                timeInterval: 1000,
+                distanceInterval: 1,
+            }, (location) => {
+                setLocation(location);
+                setRegion({
+                    ...region,
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                })
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
+
     return (
-        <View pt="$20">
-            <Center h="$80" justifyContent="center">
-                <VStack space="md" reversed={false}>
-                    <Text>Mapa</Text>
-                </VStack>
-            </Center>
+        <View>
+            {
+                errorMsg &&
+                <Text>{errorMsg}</Text>
+            }
+            {
+                location &&
+                <MapView
+                    style={{ height: '100%', width: '100%' }}
+                    region={region}
+                    onRegionChangeComplete={(region: Region) => setRegion(region)}
+                >
+                    <Marker coordinate={{
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude,
+                    }} />
+                </MapView>
+            }
         </View>
     );
 }
