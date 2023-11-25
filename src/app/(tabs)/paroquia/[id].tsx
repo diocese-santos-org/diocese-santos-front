d8e61f2d9a0fbf6d14744092b0c0bede75818368
Elián Roventini Andrade <elianroventini@outlook.com.br>
2023-11-25 16:45:40 -0300

    Paroquia Screen redesign

--- src/app/(tabs)/paroquia/[id].tsx
+++ src/app/(tabs)/paroquia/[id].tsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 import {
     View,
     Center,
     Text,
-    Heading
+    Heading,
+    ScrollView,
+    VStack
 } from "@gluestack-ui/themed";
 
-import { useLocalSearchParams } from 'expo-router';
+import { Link, useLocalSearchParams } from 'expo-router';
 
 import { getParoquia } from '@/api/DioceseSantosAPI';
 import { Paroquia } from '@/api/types/ParoquiaTypes';
@@ -31,36 +34,146 @@ export default function ParoquiasScreen() {
 
     const isNull = (value: any) => value === 'NULL' ? null : value;
 
+    const Social = (
+        { iconName, href }: { iconName: any, href: string }
+    ) =>
+        href &&
+        <Link
+            href={href}
+            style={{ marginHorizontal: 10 }}
+        >
+            <FontAwesome
+                name={iconName}
+                size={30}
+                color="black"
+            />
+        </Link>
+
+    const Contatos = () =>
+        <ScrollView
+            horizontal={true}
+            marginVertical='$4'
+        >
+            <Social
+                iconName='facebook'
+                href={isNull(paroquia?.redesSociais?.facebook)}
+            />
+            <Social
+                iconName='instagram'
+                href={isNull(paroquia?.redesSociais?.instagram)}
+            />
+            <Social
+                iconName='youtube'
+                href={isNull(paroquia?.redesSociais?.youtube)}
+            />
+            <Social
+                iconName='envelope'
+                href={isNull(paroquia?.email) ? `mailto:${paroquia?.email}` : ''}
+            />
+            <Social
+                iconName='globe'
+                href={isNull(paroquia?.urlSite)}
+            />
+        </ScrollView>
+
+    const Missas = () =>
+        <ScrollView
+            horizontal={true}
+            marginVertical='$4'
+            marginHorizontal='$8'
+        >
+            {
+                isNull(paroquia?.missas.segunda) &&
+                <VStack marginHorizontal='$4'>
+                    <Text bold>Segunda:</Text>
+                    <Text>{paroquia?.missas.segunda}</Text>
+                </VStack>
+            }
+            {
+                isNull(paroquia?.missas.terca) &&
+                <VStack marginHorizontal='$4'>
+                    <Text bold>Terça:</Text>
+                    <Text>{paroquia?.missas.terca}</Text>
+                </VStack>
+            }
+            {
+                isNull(paroquia?.missas.quarta) &&
+                <VStack marginHorizontal='$4'>
+                    <Text bold>Quarta:</Text>
+                    <Text>{paroquia?.missas.quarta}</Text>
+                </VStack>
+            }
+            {
+                isNull(paroquia?.missas.quinta) &&
+                <VStack marginHorizontal='$4'>
+                    <Text bold>Quinta:</Text>
+                    <Text>{paroquia?.missas.quinta}</Text>
+                </VStack>
+            }
+            {
+                isNull(paroquia?.missas.sexta) &&
+                <VStack marginHorizontal='$4'>
+                    <Text bold>Sexta:</Text>
+                    <Text>{paroquia?.missas.sexta}</Text>
+                </VStack>
+            }
+            {
+                isNull(paroquia?.missas.sabado) &&
+                <VStack marginHorizontal='$4'>
+                    <Text bold>Sábado:</Text>
+                    <Text>{paroquia?.missas.sabado}</Text>
+                </VStack>
+            }
+            {
+                isNull(paroquia?.missas.domingo) &&
+                <VStack marginHorizontal='$4'>
+                    <Text bold>Domingo:</Text>
+                    <Text>{paroquia?.missas.domingo}</Text>
+                </VStack>
+            }
+        </ScrollView>
+
+    const Secretaria = () =>
+        <ScrollView
+            horizontal={true}
+            margin='$4'
+        >
+            {
+                isNull(paroquia?.secretaria) &&
+                <VStack marginHorizontal='$4'>
+                    <Text bold>Secretaria:</Text>
+                    <Center>
+                        <Text pt="$2">{paroquia?.secretaria}</Text>
+                        <Text>{paroquia?.endereco.enderecoCompleto}</Text>
+                        <Text>{paroquia?.telefone}</Text>
+                    </Center>
+                </VStack>
+            }
+        </ScrollView>
+
+    const Clero = () =>
+        <ScrollView margin='$4'>
+            <Text bold>Clero:</Text>
+            {
+                paroquia?.cleros &&
+                paroquia?.cleros?.map(
+                    clero => <Text key={clero?.nome} pt="$4">{clero?.nome}</Text>
+                )
+            }
+        </ScrollView>
+
     return (
-        <View>
-            <Center justifyContent="center">
+        <View key={paroquia?.id}>
+            <Center justifyContent="center" pt='$10'>
                 <Heading size='md'>{paroquia?.nome}</Heading>
 
-                <Text pt="$4">{paroquia?.endereco.enderecoCompleto}</Text>
-                <Text pt="$4">{isNull(paroquia?.urlSite)}</Text>
-                <Text pt="$4">{paroquia?.telefone}</Text>
+                <Secretaria />
 
-                {
-                    paroquia?.cleros &&
-                    paroquia?.cleros?.map(
-                        clero => <Text pt="$4">{clero?.nome}</Text>
-                    )
-                }
+                <Missas />
 
-                <Text pt="$4">{paroquia?.missas.segunda}</Text>
-                <Text pt="$4">{paroquia?.missas.terca}</Text>
-                <Text pt="$4">{paroquia?.missas.quarta}</Text>
-                <Text pt="$4">{paroquia?.missas.quinta}</Text>
-                <Text pt="$4">{paroquia?.missas.sexta}</Text>
-                <Text pt="$4">{paroquia?.missas.sabado}</Text>
-                <Text pt="$4">{paroquia?.missas.domingo}</Text>
+                <Clero />
 
-                <Text pt="$4">{paroquia?.email}</Text>
-                <Text pt="$4">{paroquia?.secretaria}</Text>
-
-                <Text pt="$4">{isNull(paroquia?.redesSociais.facebook)}</Text>
-                <Text pt="$4">{isNull(paroquia?.redesSociais.instagram)}</Text>
-                <Text pt="$4">{isNull(paroquia?.redesSociais.youtube)}</Text>
+                <Contatos />
 
                 {/* <Text pt="$4">{distancia
                     ? distancia.toFixed(0) + ' metros'
